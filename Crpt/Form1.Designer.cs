2f3bf83cdf43f279ac32bc20c30f5def4a00c29c
ZhabaSpagettiCoder <grishazhaba@gmail.com>
2021-04-02 16:19:53 +0300

    Now i prepare to create commit by linux terminal :O

---
+++ Crpt/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Crpt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(432, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Crpt/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using Microsoft.Win32;
+namespace Crpt
+{
+    public partial class Form1 : Form
+    {
+        
+        public byte[] Key_IV_SETTER(bool a)
+        {
+            switch (a)
+            {
+                case true:
+                    {
+                        byte[] arr = new byte[32];
+                        using (FileStream fs = new FileStream("D:\\CSHRP\\KEY.txt", FileMode.Open, FileAccess.Read))
+                        {
+                            fs.Read(arr, 0, 32);
+                        }
+                        return arr;
+
+                    }
+                case false:
+                    {
+                        byte[] arr = new byte[16];
+                        using (FileStream fs = new FileStream("D:\\CSHRP\\IV.txt", FileMode.Open, FileAccess.Read))
+                        {
+                            fs.Read(arr, 0, 16);
+                        }
+                        return arr;
+                    }
+                default: { return null; }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] encrypted;
+            Aes crpt = Aes.Create();
+            crpt.IV = Key_IV_SETTER(false);
+            crpt.Key = Key_IV_SETTER(true);
+            ICryptoTransform crypt = crpt.CreateEncryptor(crpt.Key, crpt.IV);
+            
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, crypt, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(textBox1.Text);
+                    }
+                }
+                encrypted = ms.ToArray();
+                byte[] arr = encrypted.ToArray();
+                using (FileStream fs = new FileStream("D:\\CSHRP\\ENCRYPTED.txt", FileMode.Open, FileAccess.Write))
+                {
+                    fs.Write(arr,0,16);
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            Aes dcrpt = Aes.Create();
+            dcrpt.IV = Key_IV_SETTER(false);
+            dcrpt.Key = Key_IV_SETTER(true);
+            byte[] data = new byte[16];
+            using(FileStream fs = new FileStream("D:\\CSHRP\\ENCRYPTED.txt", FileMode.Open, FileAccess.Read))
+            {
+                fs.Read(data, 0, 16);
+            }
+            string DecryptedText;
+            ICryptoTransform dcrptr = dcrpt.CreateDecryptor(dcrpt.Key, dcrpt.IV);
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, dcrptr, CryptoStreamMode.Read))
+                {
+                    using (StreamReader sw = new StreamReader(cs))
+                    {
+                        DecryptedText = sw.ReadToEnd();
+                    }
+                }
+            }
+            textBox2.Text = DecryptedText;
+        }
+    }
+}
